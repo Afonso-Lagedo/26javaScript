feecdc2df283c06555876b5f9039d2b8edd8fccd
Afonso-lagedo <afonso.lagedo@gmail.com>
2026-01-27 18:51:26 -0300

    2 condicionais

---
+++ 2condicao/main.js
@@ -0,0 +1,12 @@
+//if, else if, else
+
+const agora = new Date();
+const hora = agora.getHours();
+
+if (hora >= 0 && hora <= 11) {
+  console.log("Bom dia");
+} else if (hora >= 12 && hora <= 17) {
+  console.log("Boa tarde");
+} else {
+  console.log("Boa noite");
+}
